4e9f322a1d9a1ba14ea9aaa34e2262d8f393dcca
BYEJUNG GWAG <loveakwodbs@gmail.com>
2025-05-23 16:20:54 +0900

    map 해결

---
+++ location/js/map.js
@@ -0,0 +1,20 @@
+new daum.roughmap.Lander({
+  timestamp: "1747978801926",
+  key: "2o5an",
+  mapWidth: "595",
+  mapHeight: "259",
+}).render();
+
+new daum.roughmap.Lander({
+  timestamp: "1747978437077",
+  key: "2o5ac",
+  mapWidth: "595",
+  mapHeight: "259",
+}).render();
+
+new daum.roughmap.Lander({
+  timestamp: "1747978845759",
+  key: "2o5ao",
+  mapWidth: "595",
+  mapHeight: "259",
+}).render();

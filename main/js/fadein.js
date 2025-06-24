fc58721734a25aafa23e1231880bc21be8d9973c
BYEJUNG GWAG <loveakwodbs@gmail.com>
2025-06-24 17:26:01 +0900

    애니메이션 적용중

---
+++ main/js/fadein.js
@@ -0,0 +1,49 @@
+const fadeinElements = [
+  {
+    el: document.getElementById("box-1"),
+    delay: 0,
+  },
+  {
+    el: document.getElementById("box-2"),
+    delay: 0.1,
+  },
+  {
+    el: document.getElementById("box-3"),
+    delay: 0.2,
+  },
+  // document.querySelector(".overlay"),
+];
+
+const handleScroll = ([entry], current, delay) => {
+  if (entry.isIntersecting && current) {
+    Object.assign(current.style, {
+      transitionProperty: "opacity, transform",
+      transitionDuration: "0.7s",
+      transitionTimingFunction: "ease-in-out",
+      transitionDelay: `${delay}s`,
+      opacity: "1",
+      transform: "translate3d(0, 0, 0)",
+    });
+    observer.unobserve(entry.target);
+  }
+};
+
+const observer = (current, delay) => {
+  console.log(current);
+  Object.assign(current.style, {
+    opacity: "0",
+    transform: "translate3d(0, 30%, 0)",
+  });
+
+  const intersectionObserver = new IntersectionObserver(
+    ([entry]) => handleScroll([entry], current, delay),
+    {
+      threshold: 0.2,
+    }
+  );
+  intersectionObserver.observe(current);
+};
+
+fadeinElements.forEach(({ el, delay }) => {
+  observer(el, delay);
+});
